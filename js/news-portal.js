a1f5983ec7b9e0418250ef2ce3256e79d082cf5b
Anitaferdous1907 <anitaferdous009@gmail.com>
2022-09-04 08:45:25 +0600

    error fix in nav bar

--- js/news-portal.js
+++ js/news-portal.js
@@ -1,1 +1,23 @@
-console.log("news")
+const loadNews = async () => {
+    const url = `https://openapi.programming-hero.com/api/news/categories`
+    const res = await fetch(url);
+    const data = await res.json();
+    displayNews(data.data.news_category)
+}
+const displayNews = news => {
+    //console.log(news);
+
+    const newsContainer = document.getElementById('news-categories-container');
+    news.forEach(arr_item => {
+        const newsNav = document.createElement('nav');
+        newsNav.classList.add('nav');
+        newsNav.innerHTML = `
+        
+        <li class="nav-item">
+    <a class="nav-link" href="#">${arr_item.category_name}</a>
+</li>
+    `;
+        newsContainer.appendChild(newsNav);
+    })
+}
+loadNews();
